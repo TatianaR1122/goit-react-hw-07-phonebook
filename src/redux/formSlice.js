5c6d8672f8b4453d8281cba361abbbbbfa0dbc51
TatianaRohovets <tnr78@ukr.net>
2023-05-21 13:03:17 +0300

    selectFixed

--- src/redux/formSlice.js
+++ src/redux/formSlice.js
@@ -10,52 +10,6 @@ const initState = {
   filter: '',
 };
 
-// const formSlice = createSlice({
-//   name: 'contacts',
-//   initialState: initState.contacts,
-
-//   extraReducers: {
-//     [fetchContacts.pending](state) {
-//       state.isLoading = true;
-//     },
-//     [fetchContacts.fulfilled](state, action) {
-//       state.isLoading = false;
-//       state.error = null;
-//       state.items = action.payload;
-//     },
-//     [fetchContacts.rejected](state, action) {
-//       state.isLoading = false;
-//       state.error = action.payload;
-//     },
-//     [addContact.pending](state) {
-//       state.isLoading = true;
-//     },
-//     [addContact.fulfilled](state, action) {
-//       state.isLoading = false;
-//       state.error = null;
-//       state.items.push(action.payload);
-//     },
-//     [addContact.rejected](state, action) {
-//       state.isLoading = false;
-//       state.error = action.payload;
-//     },
-//     [deleteContact.pending](state) {
-//       state.isLoading = true;
-//     },
-//     [deleteContact.fulfilled](state, action) {
-//       state.isLoading = false;
-//       state.error = null;
-//       const index = state.items.findIndex(
-//         contact => contact.id === action.payload.id
-//       );
-//       state.items.splice(index, 1);
-//     },
-//     [deleteContact.rejected](state, action) {
-//       state.isLoading = false;
-//       state.error = action.payload;
-//     },
-//   },
-// });
 const handlePending = state => {
   state.isLoading = true;
 };
@@ -67,7 +21,6 @@ const handleRejected = (state, action) => {
 
 const formSlice = createSlice({
   name: 'contacts',
-  // initialState: initState.contacts,
   initialState: initState.contacts,
 
   extraReducers: builder => {
--- src/redux/selector.js
+++ src/redux/selector.js
@@ -8,15 +8,6 @@ export const selectError = state => state.contacts.error;
 
 export const selectFilter = state => state.filter;
 
-// export const filteredContacts = state => {
-//   const contacts = selectContacts(state);
-
-//   const filtered = state.filter;
-//   return contacts.filter(contact =>
-//     contact.name.toLowerCase().includes(filtered.toLowerCase())
-//   );
-// };
-
 export const selectVisibleContacts = createSelector(
   [selectContacts, selectFilter],
   (contacts, filter) => {
